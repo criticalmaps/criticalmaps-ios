8e73ae53e59befb75b3cb5de7a5046904be6f5e7
Norman Sander <norman@nsander.de>
2015-08-08 21:51:50 +0200

    no message

--- CriticalMass/PLInfoOverlayView.m
+++ CriticalMass/PLInfoOverlayView.m
@@ -19,7 +19,7 @@
         UILabel *label = [[UILabel alloc]initWithFrame:frame];
         label.lineBreakMode = NSLineBreakByWordWrapping;
         label.numberOfLines = 0;
-        label.text = NSLocalizedString(@"maps.layer.info", nil);
+        label.text = NSLocalizedString(@"map.layer.info", nil);
         label.textColor = [UIColor whiteColor];
         label.textAlignment = NSTextAlignmentCenter;
         [self addSubview:label];
--- CriticalMass/PLTabBarController.m
+++ CriticalMass/PLTabBarController.m
@@ -55,6 +55,4 @@
     [super didReceiveMemoryWarning];
 }
 
-
-
 @end
