a661f8df2181136cec9e0ffde6bfb17af3e7d38c
Norman Sander <norman@nsander.de>
2015-08-08 21:24:48 +0200

    no message

--- CriticalMass/PLTabBarController.m
+++ CriticalMass/PLTabBarController.m
@@ -6,16 +6,9 @@
 #import "PLSettingsTableViewController.h"
 #import "UIColor+Helper.h"
 
-
-@interface PLTabBarController ()
-
-@end
-
 @implementation PLTabBarController
 
-
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
     
     self.view.tintColor = [UIColor magicColor];
@@ -31,11 +24,11 @@
     UIViewController *viewController4 = [[PLTwitterViewController alloc] init];
     UIViewController *viewController5 = [[PLSettingsTableViewController alloc] initWithStyle:UITableViewStyleGrouped];
     
-    viewController1.title = @"Karte";
-    viewController2.title = @"Knigge";
-    viewController3.title = @"Chat";
-    viewController4.title = @"Twitter";
-    viewController5.title = @"Settings";
+    viewController1.title = NSLocalizedString(@"map.title", nil);
+    viewController2.title = NSLocalizedString(@"rules.title", nil);
+    viewController3.title = NSLocalizedString(@"chat.title", nil);
+    viewController4.title = NSLocalizedString(@"twitter.title", nil);
+    viewController5.title = NSLocalizedString(@"settings.title", nil);
     
     NSMutableArray *tabViewControllers = [[NSMutableArray alloc] init];
     [tabViewControllers addObject:viewController1];
@@ -45,21 +38,20 @@
     [tabViewControllers addObject:viewController5];
     
     viewController1.tabBarItem =
-    [[UITabBarItem alloc] initWithTitle:@"Map" image:[UIImage imageNamed:@"Map"] tag:0];
+    [[UITabBarItem alloc] initWithTitle:NSLocalizedString(@"map.title", nil) image:[UIImage imageNamed:@"Map"] tag:0];
     viewController2.tabBarItem =
-    [[UITabBarItem alloc] initWithTitle:@"Knigge" image:[UIImage imageNamed:@"List"] tag:1];
+    [[UITabBarItem alloc] initWithTitle:NSLocalizedString(@"rules.title", nil) image:[UIImage imageNamed:@"List"] tag:1];
     viewController3.tabBarItem =
-    [[UITabBarItem alloc] initWithTitle:@"Chat" image:[UIImage imageNamed:@"Bubble"] tag:2];
+    [[UITabBarItem alloc] initWithTitle:NSLocalizedString(@"chat.title", nil) image:[UIImage imageNamed:@"Bubble"] tag:2];
     viewController4.tabBarItem =
-    [[UITabBarItem alloc] initWithTitle:@"Twitter" image:[UIImage imageNamed:@"Twitter"] tag:3];
+    [[UITabBarItem alloc] initWithTitle:NSLocalizedString(@"twitter.title", nil) image:[UIImage imageNamed:@"Twitter"] tag:3];
     viewController5.tabBarItem =
-    [[UITabBarItem alloc] initWithTitle:@"Settings" image:[UIImage imageNamed:@"Settings"] tag:4];
+    [[UITabBarItem alloc] initWithTitle:NSLocalizedString(@"settings.title", nil) image:[UIImage imageNamed:@"Settings"] tag:4];
     
     [self setViewControllers:tabViewControllers animated:NO];
 }
 
-- (void)didReceiveMemoryWarning
-{
+- (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
 }
 
