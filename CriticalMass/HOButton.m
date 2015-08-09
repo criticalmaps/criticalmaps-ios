8896d62526020785c6f6613a7398f3253f456bfa
Norman Sander <norman@nsander.de>
2015-08-09 22:07:24 +0200

    Remove some warnings

--- CriticalMass/HOButton.m
+++ CriticalMass/HOButton.m
@@ -2,19 +2,16 @@
 
 @implementation HOButton
 
-- (id)initWithFrame:(CGRect)frame
-{
+- (id)initWithFrame:(CGRect)frame {
     self = [super initWithFrame:frame];
     if (self) {
-        
-        self = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+        self = (HOButton *)[UIButton buttonWithType:UIButtonTypeRoundedRect];
         CGRect buttonFrame = self.frame;
         buttonFrame.size = CGSizeMake(40, 40);
         self.frame = buttonFrame;
         self.layer.cornerRadius = 3;
         [self.layer setBorderColor:[[UIColor lightGrayColor] CGColor]];
         [self.layer setBorderWidth:1.0f];
-        //[self setTintColor:[UIColor blueColor]];
     }
     return self;
 }
