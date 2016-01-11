dcc9246a8e5b47bf71769634645a7ec5de61a488
Norman Sander <norman@nsander.de>
2016-01-11 17:32:57 +0100

    Minor fixes; Incremented build number

--- CriticalMass/PLDataModel.m
+++ CriticalMass/PLDataModel.m
@@ -74,7 +74,7 @@
 - (void)initHTTPRequestManager {
     _operationManager = [AFHTTPRequestOperationManager manager];
     _operationManager.requestSerializer = [AFJSONRequestSerializer serializer];
-    _operationManager.responseSerializer.acceptableContentTypes = [NSSet setWithObject:@"application/json"];
+    _operationManager.responseSerializer.acceptableContentTypes = [NSSet setWithObjects:@"application/json", @"text/json", @"text/javascript", @"text/html", nil];
 }
 
 - (void)startRequestInterval {
