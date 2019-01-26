964f9702899c736c30506634225dc132408e2aa4
Leonard Thomas <leo.thomas@me.com>
2019-01-26 20:22:30 +0100

    Move chatInput with keyboard

--- CriticalMass/ChatViewController.swift
+++ CriticalMass/ChatViewController.swift
@@ -8,9 +8,12 @@
 import UIKit
 
 class ChatViewController: UIViewController, ChatInputDelegate {
-    let chatInput = ChatInputView(frame: .zero)
-    let messagesTableViewController = MessagesTableViewController<ChatMessageTableViewCell>(style: .plain)
-    let chatManager: ChatManager
+    private let chatInput = ChatInputView(frame: .zero)
+    private let messagesTableViewController = MessagesTableViewController<ChatMessageTableViewCell>(style: .plain)
+    private let chatManager: ChatManager
+    private lazy var chatInputBottomConstraint = {
+        NSLayoutConstraint(item: chatInput, attribute: .bottom, relatedBy: .equal, toItem: view, attribute: .bottomMargin, multiplier: 1, constant: 0)
+    }()
 
     @objc init(chatManager: ChatManager) {
         self.chatManager = chatManager
@@ -24,9 +27,15 @@ class ChatViewController: UIViewController, ChatInputDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        configureNotifications()
         configureNavigationBar()
         configureChatInput()
         configureMessagesTableViewController()
+    }
+
+    private func configureNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
 
     private func configureNavigationBar() {
@@ -68,12 +77,36 @@ class ChatViewController: UIViewController, ChatInputDelegate {
             NSLayoutConstraint(item: chatInput, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 64),
             NSLayoutConstraint(item: chatInput, attribute: .width, relatedBy: .equal, toItem: view, attribute: .width, multiplier: 1, constant: 0),
             NSLayoutConstraint(item: chatInput, attribute: .centerX, relatedBy: .equal, toItem: view, attribute: .centerX, multiplier: 1, constant: 0),
-            NSLayoutConstraint(item: chatInput, attribute: .bottom, relatedBy: .equal, toItem: view, attribute: .bottomMargin, multiplier: 1, constant: 0),
+            chatInputBottomConstraint,
         ])
     }
 
     @objc private func didTapTableView() {
         chatInput.resignFirstResponder()
+    }
+
+    // MARK: Keyboard Handling
+
+    @objc private func keyboardWillShow(notification: Notification) {
+        let duration = notification.userInfo![UIResponder.keyboardAnimationDurationUserInfoKey] as! Double
+        let curve = notification.userInfo![UIResponder.keyboardAnimationCurveUserInfoKey] as! UInt
+        let beginFrame = (notification.userInfo![UIResponder.keyboardFrameBeginUserInfoKey] as! NSValue).cgRectValue
+        let endFrame = (notification.userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+
+        chatInputBottomConstraint.constant = (endFrame.minY - beginFrame.minY) + (view.frame.maxY - chatInput.frame.maxY)
+        UIView.animate(withDuration: duration, delay: 0, options: UIView.AnimationOptions(rawValue: curve), animations: {
+            self.view.layoutIfNeeded()
+        }, completion: nil)
+    }
+
+    @objc private func keyboardWillHide(notification: Notification) {
+        let duration = notification.userInfo![UIResponder.keyboardAnimationDurationUserInfoKey] as! Double
+        let curve = notification.userInfo![UIResponder.keyboardAnimationCurveUserInfoKey] as! UInt
+
+        chatInputBottomConstraint.constant = 0
+        UIView.animate(withDuration: duration, delay: 0, options: UIView.AnimationOptions(rawValue: curve), animations: {
+            self.view.layoutIfNeeded()
+        }, completion: nil)
     }
 
     // MARK: ChatInputDelegate
