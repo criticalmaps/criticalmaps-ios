62dfa234b0dc3ef1bb0326c9459fa83797c5a9d5
Malte Bünz <malte.buenz@deutschebahn.com>
2019-03-13 17:37:48 +0100

    Refactor + inner border/shadow
    * adds innerShadow
    * adds a little bit more readabiliy 🎨

--- CriticalMass/RoundableImageView.swift
+++ CriticalMass/RoundableImageView.swift
@@ -12,53 +12,66 @@ import UIKit
 class RoundableImageView: UIImageView {
     
     @IBInspectable var cornerRadius : CGFloat = 0.0 {
-        didSet{
+        didSet {
             self.applyCornerRadius()
         }
     }
     
     @IBInspectable var borderColor : UIColor = UIColor.clear {
-        didSet{
+        didSet {
             self.applyCornerRadius()
         }
     }
     
-    @IBInspectable var borderWidth : Double = 0 {
-        didSet{
+    @IBInspectable var borderWidth : Double = 0.0 {
+        didSet {
             self.applyCornerRadius()
         }
     }
     
     @IBInspectable var circular : Bool = false {
-        didSet{
+        didSet {
             self.applyCornerRadius()
         }
     }
     
-    func applyCornerRadius()
-    {
-        if(self.circular) {
-            self.layer.cornerRadius = self.bounds.size.height/2
-            self.layer.masksToBounds = true
-            self.layer.borderColor = self.borderColor.cgColor
-            self.layer.borderWidth = CGFloat(self.borderWidth)
-        }else {
+    private func applyCornerRadius() {
+        if self.circular {
+            self.layer.cornerRadius = self.bounds.size.height / 2
+        } else {
             self.layer.cornerRadius = cornerRadius
-            self.layer.masksToBounds = true
-            self.layer.borderColor = self.borderColor.cgColor
-            self.layer.borderWidth = CGFloat(self.borderWidth)
         }
+        self.layer.masksToBounds = true
+        self.layer.borderColor = self.borderColor.cgColor
+        self.layer.borderWidth = CGFloat(self.borderWidth)
+    }
+    
+    private func addInnerShadow() {
+        let innerShadow = CALayer()
+        innerShadow.frame = bounds
+        let path = UIBezierPath(ovalIn: self.bounds.insetBy(dx: -1, dy: -1))
+        let cutout = UIBezierPath(ovalIn: self.bounds).reversing()
+        path.append(cutout)
+        innerShadow.shadowPath = path.cgPath
+        innerShadow.masksToBounds = true
+        // Shadow properties
+        innerShadow.shadowColor = UIColor.twitterProfileInnerBorder.cgColor
+        innerShadow.shadowOffset = .zero
+        innerShadow.shadowOpacity = 1.0
+        innerShadow.shadowRadius = 0.0
+        // Add
+        layer.addSublayer(innerShadow)
     }
     
     override func awakeFromNib() {
         super.awakeFromNib()
         self.applyCornerRadius()
+        self.addInnerShadow()
     }
     
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        applyCornerRadius()
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        self.applyCornerRadius()
+        self.addInnerShadow()
     }
-    
 }
-
