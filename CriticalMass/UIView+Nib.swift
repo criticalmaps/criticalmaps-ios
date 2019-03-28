40abdb1e764178e77df7fb96ed6b417f7c1231f2
Malte Bünz <malte.buenz@posteo.de>
2019-03-28 18:27:24 +0100

    Settings Footer View
    * adds CM footer to settings
    * adds hasInnerShadow property to CircularImageView
    * sets footer in settings
    * adds NibInstantiatable protocol
    * adds Bundle Extension to get bundle and versionNumber

---
+++ CriticalMass/UIView+Nib.swift
@@ -0,0 +1,32 @@
+//
+//  UIView+Nib.swift
+//  CriticalMaps
+//
+//  Created by Malte Bünz on 28.03.19.
+//  Copyright © 2019 Pokus Labs. All rights reserved.
+//
+
+import UIKit
+
+protocol NibInstantiatable {
+    static func nibName() -> String
+}
+
+extension NibInstantiatable {
+    static func nibName() -> String {
+        return String(describing: self)
+    }
+}
+
+extension NibInstantiatable where Self: UIView {
+    
+    @discardableResult
+    static func fromNib() -> Self? {
+        let bundle = Bundle(for: self)
+        let nib = bundle.loadNibNamed(nibName(), owner: self, options: nil)
+        guard let view = nib?.first as? Self else {
+            return nil
+        }
+        return view
+    }
+}
