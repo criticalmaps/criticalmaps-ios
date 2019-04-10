76fdc9a59a4f8d39698eecaf11948f229e0af02f
Malte Bünz <malte.buenz@posteo.de>
2019-04-10 09:45:26 +0200

    add Theme

---
+++ CriticalMass/Theme.swift
@@ -0,0 +1,25 @@
+//
+//  Theme.swift
+//  CriticalMaps
+//
+//  Created by Malte Bünz on 10.04.19.
+//  Copyright © 2019 Pokus Labs. All rights reserved.
+//
+
+import Foundation
+
+enum Theme: Int {
+    case light
+    case dark
+    
+    var style: ThemeDefining {
+        switch self {
+        case .light:
+            return LightTheme()
+        case .dark:
+            return DarkTheme()
+        }
+    }
+}
+
+
