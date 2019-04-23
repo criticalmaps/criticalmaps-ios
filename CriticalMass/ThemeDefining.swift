74c763c2b62603cd8a901454935355b88138a610
Malte Bünz <malte.buenz@posteo.de>
2019-04-23 21:41:28 +0200

    add styles

--- CriticalMass/ThemeDefining.swift
+++ CriticalMass/ThemeDefining.swift
@@ -6,8 +6,8 @@
 //  Copyright © 2019 Pokus Labs. All rights reserved.
 //
 
-import UIKit
 import Foundation
+import UIKit
 
 protocol ThemeDefining {
     var name: String { get }
@@ -18,40 +18,64 @@ protocol ThemeDefining {
     var secondaryBackgroundColor: UIColor { get }
     var negativeBackgroundColor: UIColor { get }
     var titleTextColor: UIColor { get }
+    var secondaryTitleTextColor: UIColor { get }
     var switchTintColor: UIColor { get }
     var refreshControlColor: UIColor { get }
+    var chatMessageInputTextViewBackgroundColor: UIColor { get }
+    var navigationOverlaySeperatorColor: UIColor { get }
 }
 
 struct LightTheme: ThemeDefining {
     var name: String {
         return NSLocalizedString("settings.theme.light", comment: "")
     }
+
     var tintColor: UIColor {
         return .blue
     }
+
     var barStyle: UIBarStyle {
         return .default
     }
+
     var keyboardAppearance: UIKeyboardAppearance {
         return .default
     }
+
     var backgroundColor: UIColor {
         return .white
     }
+
     var secondaryBackgroundColor: UIColor {
         return .white
     }
+
     var negativeBackgroundColor: UIColor {
         return .blue
     }
+
     var titleTextColor: UIColor {
         return .black
     }
+
+    var secondaryTitleTextColor: UIColor {
+        return .gray300
+    }
+
     var switchTintColor: UIColor {
         return UIColor(red: 0.30, green: 0.85, blue: 0.39, alpha: 1.00)
     }
+
     var refreshControlColor: UIColor {
         return .black
+    }
+
+    var chatMessageInputTextViewBackgroundColor: UIColor {
+        return .gray300
+    }
+
+    var navigationOverlaySeperatorColor: UIColor {
+        return UIColor(red: 0.86, green: 0.88, blue: 0.85, alpha: 1.00)
     }
 }
 
@@ -59,31 +83,52 @@ struct DarkTheme: ThemeDefining {
     var name: String {
         return NSLocalizedString("settings.theme.dark", comment: "")
     }
+
     var tintColor: UIColor {
         return .yellow100
     }
+
     var barStyle: UIBarStyle {
         return .black
     }
+
     var keyboardAppearance: UIKeyboardAppearance {
         return .dark
     }
+
     var backgroundColor: UIColor {
         return .gray200
     }
+
     var secondaryBackgroundColor: UIColor {
         return .black
     }
+
+    var secondaryTitleTextColor: UIColor {
+        return .gray500
+    }
+
     var negativeBackgroundColor: UIColor {
         return .white
     }
+
     var titleTextColor: UIColor {
         return .white
     }
+
     var switchTintColor: UIColor {
         return tintColor
     }
+
     var refreshControlColor: UIColor {
         return .yellow100
+    }
+
+    var chatMessageInputTextViewBackgroundColor: UIColor {
+        return .gray100
+    }
+
+    var navigationOverlaySeperatorColor: UIColor {
+        return UIColor(red: 0.22, green: 0.22, blue: 0.22, alpha: 1.00)
     }
 }
