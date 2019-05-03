7adc46ebfff7f00298f1b3ee630842fcefc23fcf
Malte Bünz <malte.buenz@posteo.de>
2019-05-03 19:16:04 +0200

    * remove unused colors * add gray600 * standardize color property structure

--- CriticalMass/Colors.swift
+++ CriticalMass/Colors.swift
@@ -17,7 +17,7 @@ extension UIColor {
     }
 
     static var gray200: UIColor {
-        return UIColor(red: 38 / 255.0, green: 38 / 255.0, blue: 38 / 255.0, alpha: 1)
+        return UIColor(white: 38.0 / 255.0, alpha: 1)
     }
 
     static var gray300: UIColor {
@@ -32,43 +32,31 @@ extension UIColor {
         return UIColor(red: 218.0 / 255.0, green: 220.0 / 255.0, blue: 224.0 / 255.0, alpha: 1.0)
     }
 
-    static var settingsForeground: UIColor {
-        return UIColor(red: 38 / 255.0, green: 38 / 255.0, blue: 38 / 255.0, alpha: 1)
-    }
-
     static var settingsOpenSourceForeground: UIColor {
-        return UIColor(red: 26 / 255.0, green: 26 / 255.0, blue: 26 / 255.0, alpha: 1)
+        return UIColor(white: 26.0 / 255.0, alpha: 1)
     }
 
     static var navigationOverlayBackground: UIColor {
-        return UIColor(red: 255 / 255.0, green: 255 / 255.0, blue: 255 / 255.0, alpha: 0.8)
-    }
-
-    static var navigationOverlayForeground: UIColor {
-        return UIColor(red: 38 / 255.0, green: 38 / 255.0, blue: 38 / 255.0, alpha: 1)
-    }
-
-    static var navigationOverlaySeparator: UIColor {
-        return UIColor(white: 0.0, alpha: 0.1)
-    }
-
-    static var socialTabControlTintColor: UIColor {
-        return .black
+        return UIColor(white: 1.0, alpha: 0.8)
     }
 
     static var yellow100: UIColor {
-        return UIColor(red: 1.0, green: 214.0 / 255.0, blue: 51.0 / 255.0, alpha: 1.0)
+        return UIColor(red: 255.0 / 255.0, green: 214.0 / 255.0, blue: 51.0 / 255.0, alpha: 1.0)
+    }
+
+    static var gray600: UIColor {
+        return UIColor(white: 250.0 / 255.0, alpha: 1.0)
     }
 
     static var lightThemeToolBarBackgroundColor: UIColor {
-        return UIColor(red: 0.98, green: 0.98, blue: 0.98, alpha: 1.00)
+        return gray600
     }
 
     static var lightThemeNavigationOverlaySeperatorColor: UIColor {
-        return UIColor(red: 0.86, green: 0.88, blue: 0.85, alpha: 1.00)
+        return UIColor(red: 220.0 / 255.0, green: 224.0 / 255.0, blue: 216.0 / 255.0, alpha: 1.00)
     }
 
     static var darkThemeNavigationOverlaySeperatorColor: UIColor {
-        return UIColor(red: 0.22, green: 0.22, blue: 0.22, alpha: 1.00)
+        return UIColor(white: 57.0 / 255.0, alpha: 1.00)
     }
 }
