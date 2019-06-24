7686aa4c4f56f3d7516db2cc09391d1dfad742b2
Malte Bünz <malte.buenz@posteo.de>
2019-06-24 08:19:15 +0200

    add viewController management extension

---
+++ CriticalMass/UIViewController+Management.swift
@@ -0,0 +1,25 @@
+//
+//  UIViewController+Management.swift
+//  CriticalMaps
+//
+//  Created by Malte Bünz on 24.06.19.
+//  Copyright © 2019 Pokus Labs. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+    func add(child viewController: UIViewController) {
+        addChild(viewController)
+        view.addSubview(viewController.view)
+        viewController.view.frame = view.bounds
+        viewController.view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        viewController.didMove(toParent: self)
+    }
+
+    func remove(child viewController: UIViewController) {
+        viewController.willMove(toParent: nil)
+        viewController.view.removeFromSuperview()
+        viewController.removeFromParent()
+    }
+}
