675be69979332628b55ad8c372eaf5f095e1fc21
Malte Buenz <malte.buenz@posteo.de>
2019-07-20 17:51:22 +0200

    add plugin extensions

--- CriticalMass/UIViewController+Management.swift
+++ CriticalMass/UIViewController+Management.swift
@@ -23,3 +23,23 @@ extension UIViewController {
         viewController.removeFromParent()
     }
 }
+
+extension UIViewController {
+    func add(_ child: UIViewController) {
+        addChild(child)
+        view.addSubview(child.view)
+        child.didMove(toParent: self)
+    }
+    
+    func remove() {
+        // Just to be safe, we check that this view controller
+        // is actually added to a parent before removing it.
+        guard parent != nil else {
+            return
+        }
+        
+        willMove(toParent: nil)
+        view.removeFromSuperview()
+        removeFromParent()
+    }
+}
