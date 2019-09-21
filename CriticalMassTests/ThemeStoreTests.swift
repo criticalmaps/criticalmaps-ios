d6be21947101239e5133bd86e3f47779225cc8fb
Malte Buenz <malte.buenz@posteo.de>
2019-09-21 19:11:40 +0200

    remove somewhat weird test

--- CriticalMassTests/ThemeStoreTests.swift
+++ CriticalMassTests/ThemeStoreTests.swift
@@ -13,45 +13,11 @@ class MockThemeStore: ThemeStorable {
     private var currentTheme: Theme?
 
     func load() -> Theme? {
-        guard let theme = currentTheme else {
-            return .light
-        }
-        return theme
+        return currentTheme
     }
 
     func save(_ themeSelection: Theme) {
         currentTheme = themeSelection
-    }
-}
-
-class MockThemeStoreTests: XCTestCase {
-    var sut: ThemeStorable?
-
-    override func setUp() {
-        super.setUp()
-        sut = MockThemeStore()
-    }
-
-    override func tearDown() {
-        sut = nil
-        super.tearDown()
-    }
-
-    func testStoreShouldReturnLightThemeWhenNothingIsSavedBefore() {
-        // given
-        let theme = sut!.load()
-        // then
-        XCTAssertEqual(theme, .light)
-    }
-
-    func testStoreShouldReturnDarkThemeWhenDarkIsSavedBefore() {
-        // given
-        let theme = Theme.dark
-        // when
-        sut?.save(theme)
-        let loadedTheme = sut!.load()
-        // then
-        XCTAssertEqual(loadedTheme, .dark)
     }
 }
 
