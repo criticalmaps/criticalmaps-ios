2d3b6cddde5a0317dd0330a2cfa8559198ff477a
Leonard Thomas <leo.thomas@me.com>
2019-09-24 18:03:05 +0200

    small cleanup

--- CriticalMass/QRCodeView.swift
+++ CriticalMass/QRCodeView.swift
@@ -9,14 +9,6 @@
 import UIKit
 
 class QRCodeView: UIView {
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-    }
-
-    required init?(coder _: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-
     var text: String? {
         didSet {
             filter.setValue(text?.data(using: .utf8), forKey: "inputMessage")
