52daf815514e9a087213cd05921622963a6dcc3a
Felizia Bernutz <fbe@adorsys.de>
2019-10-06 10:47:56 +0200

    Add UITest and add accessibility identifier for buttons

--- CriticalMapsUITests/CriticalMapsUITests.swift
+++ CriticalMapsUITests/CriticalMapsUITests.swift
@@ -11,33 +11,52 @@ import XCTest
 class CriticalMapsUITests: XCTestCase {
 
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
+        super.setUp()
         continueAfterFailure = false
 
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // UI tests must launch the application that they test.
         let app = XCUIApplication()
+        setupSnapshot(app, waitForAnimations: false)
         app.launch()
-
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
-    func testLaunchPerformance() {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
-                XCUIApplication().launch()
-            }
+    func testScreenshots() {
+        let app = XCUIApplication()
+        snapshot("map")
+
+        let closeButton = app.navigationBars.buttons.element(boundBy: 0).firstMatch
+        let backButton = closeButton
+
+        XCTContext.runActivity(named: "Show Chat") { _ in
+            app.buttons["Chat"].tap()
+            snapshot("chat")
+            closeButton.tap()
+        }
+
+        XCTContext.runActivity(named: "Show Rules") { _ in
+            app.buttons["Rules"].tap()
+            snapshot("rules")
+
+            let firstCell = app.tables.element(boundBy: 0).cells.element(boundBy: 0).firstMatch
+            firstCell.tap()
+            snapshot("rules_corken")
+
+            backButton.tap()
+            closeButton.tap()
+        }
+
+        XCTContext.runActivity(named: "Show Settings") { _ in
+            app.buttons["Settings"].tap()
+            snapshot("settings")
+            closeButton.tap()
         }
     }
+
+//    func testLaunchPerformance() {
+//        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
+//            // This measures how long it takes to launch your application.
+//            measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
+//                XCUIApplication().launch()
+//            }
+//        }
+//    }
 }
