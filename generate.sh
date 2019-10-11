9fa94323f01ec8c81824aaea193609753109c4f8
Ravi Aggarwal <ravi.aggarwal61@gmail.com>
2019-10-11 15:08:55 +0530

    updated script to download release assets instead of running them

--- generate.sh
+++ generate.sh
@@ -19,13 +19,42 @@ check_bundle() {
 }
 
 run_swiftformat() {
-    ./bin/swiftformat .
+    echo "Creating a folder for swiftformat download meta"
+    mkdir extract_swiftformat
+    cd extract_swiftformat
+
+    echo "Downloading swiftformat executable"
+    curl -Ls https://api.github.com/repos/nicklockwood/Swiftformat/zipball > swiftformat.zip
+
+    unzip -q swiftformat.zip
+
+    echo "Forcefully copying the extracted swiftformat executble to bin"
+    yes | cp -rf nicklockwood*/CommandLineTool/swiftformat ../bin/
+
+    echo "Removing the created swiftformat meta folder"
+    cd ..
+    rm -rf extract_swiftformat
 }
 
 run_license_plist() {
-    ./bin/license-plist
-    cp -fR ./com.mono0926.LicensePlist.Output/* ./Settings.bundle
-    rm -rf ./com.mono0926.LicensePlist.Output
+    echo "Creating a folder for license-plist download meta"
+    mkdir extract_license_plist
+    cd extract_license_plist
+
+    echo "Downloading license-plist executable"
+    curl -Ls https://api.github.com/repos/mono0926/LicensePlist/releases/latest \
+    | grep "browser_download_url.*portable_licenseplist.zip" | cut -d : -f 2,3 \
+    | tr -d \" \
+    | wget -qi -
+
+    unzip -q portable_licenseplist.zip
+
+    echo "Forcefully copying the extracted license-plist executble to bin"
+    yes | cp -rf ./license-plist ../bin/
+
+    echo "Removing the created license-plist meta folder"
+    cd ..
+    rm -rf extract_license_plist
 }
 
 check_bundle;
