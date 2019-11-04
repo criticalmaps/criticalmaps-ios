b1de3bcebd9fac61828e41ce51a8f5e28051a0f5
Malte Buenz <malte.buenz@posteo.de>
2019-11-04 11:26:44 +0100

    add ErrorStateViewController

---
+++ CriticalMass/ErrorStateViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ErrorStateViewController.swift
+//  CriticalMaps
+//
+//  Created by Malte Bünz on 04.11.19.
+//  Copyright © 2019 Pokus Labs. All rights reserved.
+//
+
+import UIKit
+
+class ErrorStateViewController: UIViewController {
+    @IBOutlet private weak var errorTitle: UILabel!
+    @IBOutlet private weak var errorMessage: UILabel!
+
+    var reloadHandler: () -> Void = {}
+    var errorStateModel: ErrorStateModel! {
+        didSet {
+            errorTitle.text = errorStateModel.errorTitle
+            errorMessage.text = errorStateModel.errorMessage
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func reload(_ sender: Any) {
+        reloadHandler()
+    }
+}
+
+extension ErrorStateViewController {
+    static func createErrorStateController(with viewModel: ErrorStateModel) -> ErrorStateViewController {
+        let controller = ErrorStateViewController(nibName: String(describing: ErrorStateViewController.self), bundle: Bundle(for: ErrorStateViewController.self))
+        controller.errorStateModel = viewModel
+        return controller
+    }
+
+    static let `default` = createErrorStateController(with: .default)
+}
+
+struct ErrorStateModel {
+    let errorTitle: String
+    let errorMessage: String
+}
+
+extension ErrorStateModel {
+    static let `default` = ErrorStateModel(errorTitle: String.errorStateTitle,
+                                           errorMessage: String.errorStateMessage)
+}
