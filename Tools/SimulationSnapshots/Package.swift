09a013885d54cde3ec696c7e1f48a78ecce4e22c
Leonard Thomas <leo.thomas@me.com>
2019-11-10 12:44:46 +0100

    swiftformat

--- Tools/SimulationSnapshots/Package.swift
+++ Tools/SimulationSnapshots/Package.swift
@@ -6,18 +6,19 @@ import PackageDescription
 let package = Package(
     name: "SimulationSnapshots",
     platforms: [
-        .macOS(.v10_12)
+        .macOS(.v10_12),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(path:  "../../"),
-        .package(url: "https://github.com/hartbit/Yaap.git", from: "1.0.0")
+        .package(path: "../../"),
+        .package(url: "https://github.com/hartbit/Yaap.git", from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SimulationSnapshots",
-            dependencies: ["CriticalMapsKit", "Yaap"])
+            dependencies: ["CriticalMapsKit", "Yaap"]
+        ),
     ]
 )
--- Tools/SimulationSnapshots/Sources/SimulationSnapshots/main.swift
+++ Tools/SimulationSnapshots/Sources/SimulationSnapshots/main.swift
@@ -1,10 +1,10 @@
 import CriticalMapsKit
-import Yaap
 import Foundation
+import Yaap
 
 struct NoLocationProvider: LocationProvider {
     var currentLocation: Location?
-    
+
     static var accessPermission: LocationProviderPermission {
         return .disabled
     }
@@ -12,11 +12,11 @@ struct NoLocationProvider: LocationProvider {
 
 struct NoIdProvider: IDProvider {
     var token: String = ""
-    
-    static func hash(id: String, currentDate: Date) -> String {
+
+    static func hash(id: String, currentDate _: Date) -> String {
         return id
     }
-    
+
     var id: String = ""
 }
 
@@ -24,28 +24,28 @@ class SaveToDiskStore: DataStore {
     var outputPath: String
     var outputStream: TextOutputStream
     var errorStream: TextOutputStream
-    
+
     var snapshotCount = 0
-    
+
     init(outputPath: String, outputStream: TextOutputStream, errorStream: TextOutputStream) {
         self.outputPath = outputPath
         self.outputStream = outputStream
         self.errorStream = errorStream
     }
-    
+
     func update(with response: ApiResponse) {
         guard let data = try? JSONEncoder().encode(response) else {
             errorStream.write("Couldn't encode data")
             return
         }
-        
+
         let url = URL(fileURLWithPath: outputPath)
             .appendingPathComponent("snapshot_\(snapshotCount)")
             .appendingPathExtension("json")
-        
+
         do {
             try data.write(to: url)
-            
+
             snapshotCount += 1
             outputStream.write("💾 Saved snapshot nr. \(snapshotCount)")
             outputStream.write("\n")
@@ -63,12 +63,12 @@ class GenerateSnapshotCommand: Command {
 
     @Argument(documentation: "The output path")
     var output: String
-    
+
     // TODO: add support for timeframes and other arguments
-    
+
     func run(outputStream: inout TextOutputStream, errorStream: inout TextOutputStream) throws {
-        let store = SaveToDiskStore(outputPath: output, outputStream: outputStream,  errorStream: errorStream)
-        
+        let store = SaveToDiskStore(outputPath: output, outputStream: outputStream, errorStream: errorStream)
+
         let configuration = URLSessionConfiguration.default
         configuration.requestCachePolicy = .reloadIgnoringLocalCacheData
         configuration.timeoutIntervalForRequest = 15.0
