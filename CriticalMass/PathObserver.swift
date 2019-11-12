a7b297175c87654531a6481ca05fb457c80c2397
Ilya Glushchuk <musicguy675@gmail.com>
2019-11-12 19:16:23 +1000

    Implement PathObserver

---
+++ CriticalMass/PathObserver.swift
@@ -0,0 +1,25 @@
+//
+//  PathObserver.swift
+//  CriticalMaps
+//
+//  Created by Илья Глущук on 12.11.2019.
+//  Copyright © 2019 Pokus Labs. All rights reserved.
+//
+
+import Network
+
+@available(iOS 12.0, *)
+final class PathObserver: NetworkStatusObserver {
+    var networkStatus: NetworkStatus {
+        monitor.currentPath.status == .satisfied ? .satisfied : .none
+    }
+
+    var statusUpdateHandler: ((NetworkStatus) -> Void)?
+
+    private let monitor: NWPathMonitor = NWPathMonitor()
+
+    init() {
+        let queue = DispatchQueue(label: "Monitor")
+        monitor.start(queue: queue)
+    }
+}
