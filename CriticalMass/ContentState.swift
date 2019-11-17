459ba563a2eef14e667059955e7cdba1bfa97102
Malte Buenz <malte.buenz@posteo.de>
2019-11-17 13:41:51 +0100

    add topAnchorMargin to stateViewController layout

--- CriticalMass/ContentState.swift
+++ CriticalMass/ContentState.swift
@@ -16,16 +16,16 @@ enum ContentState<T> {
 
 protocol ContentStatePresentable {
     var contentStateViewController: UIViewController? { get set }
-    func addAndLayoutStateView(_ viewController: UIViewController, in view: UIView)
+    func addAndLayoutStateView(_ viewController: UIViewController, in view: UIView, topAnchorMargin: CGFloat)
 }
 
 extension ContentStatePresentable where Self: UIViewController {
-    func addAndLayoutStateView(_ viewController: UIViewController, in view: UIView) {
+    func addAndLayoutStateView(_ viewController: UIViewController, in view: UIView, topAnchorMargin: CGFloat = 0.0) {
         add(viewController)
         let contentStateView = viewController.view!
         contentStateView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            contentStateView.topAnchor.constraint(equalTo: view.topAnchor),
+            contentStateView.topAnchor.constraint(equalTo: view.topAnchor, constant: topAnchorMargin),
             contentStateView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
             contentStateView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             contentStateView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
