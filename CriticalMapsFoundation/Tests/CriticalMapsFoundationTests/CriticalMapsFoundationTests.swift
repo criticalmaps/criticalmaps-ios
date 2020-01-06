f29fff8236d0836b692eb917af361461b31727c5
Leonard Thomas <leo.thomas@me.com>
2020-01-06 12:47:54 +0100

    removed dead test cases

--- CriticalMapsFoundation/Tests/CriticalMapsFoundationTests/CriticalMapsFoundationTests.swift
+++ CriticalMapsFoundation/Tests/CriticalMapsFoundationTests/CriticalMapsFoundationTests.swift
@@ -6,7 +6,7 @@ final class CriticalMapsFoundationTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(CriticalMapsFoundation().text, "Hello, World!")
+        // XCTAssertEqual(CriticalMapsFoundation().text, "Hello, World!")
     }
 
     static var allTests = [
--- CriticalMapsKit/Tests/CriticalMapsKitTests/CriticalMapsKitTests.swift
+++ CriticalMapsKit/Tests/CriticalMapsKitTests/CriticalMapsKitTests.swift
@@ -6,7 +6,7 @@ final class CriticalMapsKitTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(CriticalMapsKit().text, "Hello, World!")
+        // XCTAssertEqual(CriticalMapsKit().text, "Hello, World!")
     }
 
     static var allTests = [
