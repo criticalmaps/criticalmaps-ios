2d9142d11adcffc6ea7ba701c8dfd53c00b37d14
Malte Buenz <malte.buenz@posteo.de>
2020-01-12 13:06:58 +0100

    update makeRequest to use URLComponents

--- CriticalMass/APIRequest.swift
+++ CriticalMass/APIRequest.swift
@@ -20,8 +20,20 @@ extension APIRequestDefining {
         false
     }
 
-    func makeRequest() -> URLRequest {
-        var request = URLRequest(url: endpoint.url)
+    func makeRequest() throws -> URLRequest {
+        var components = URLComponents()
+        components.scheme = "https"
+        components.host = endpoint.baseUrl
+        if let path = endpoint.path {
+            components.path = path
+        }
+        if let queryItems = getQueryItems() {
+            components.queryItems = queryItems
+        }
+        guard let url = components.url else {
+            throw APIRequestBuildError.invalidURL
+        }
+        var request = URLRequest(url: url)
         request.httpMethod = httpMethod.rawValue
         request.addHeaders(headers)
         return request
