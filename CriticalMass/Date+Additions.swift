c0a37e3e5ff955cf84d181b71ceb488a3d4024d3
Malte Buenz <malte.buenz@posteo.de>
2020-01-20 22:13:34 +0100

    update static yesterday var

--- CriticalMass/Date+Additions.swift
+++ CriticalMass/Date+Additions.swift
@@ -4,6 +4,20 @@
 import Foundation
 
 public extension Date {
-    static var yesterday: Date { Date(timeInterval: -86400, since: .now) }
+    static var yesterday: Date {
+        let dateComponents = DateComponents(day: -1)
+        return Calendar.current.date(byAdding: dateComponents, to: .now)!
+    }
+
     static var now: Date { Date() }
+
+    /// Get a component representation of todays Date as Int.
+    /// - Parameter keyPath:
+    /// - Returns: DateComponent representation as Int. Returns 0 when component is not available
+    static func getCurrent(_ keyPath: KeyPath<DateComponents, Int?>) -> Int {
+        let calendar = Calendar.current
+        let components = calendar.dateComponents([.year, .month, .day], from: .now)
+        let component = components[keyPath: keyPath] ?? 0
+        return component
+    }
 }
