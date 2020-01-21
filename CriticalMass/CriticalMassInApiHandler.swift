e83124f6018ac54d75644be861fec04d22f4ae67
Malte Buenz <malte.buenz@posteo.de>
2020-01-21 11:59:59 +0100

    update apiHander

--- CriticalMass/CriticalMassInApiHandler.swift
+++ CriticalMass/CriticalMassInApiHandler.swift
@@ -1,10 +1,5 @@
 //
-//  CriticalMassInApiHandler.swift
 //  CriticalMaps
-//
-//  Created by Malte Bünz on 12.01.20.
-//  Copyright © 2020 Pokus Labs. All rights reserved.
-//
 
 import CoreLocation
 import Foundation
@@ -12,7 +7,7 @@ import Foundation
 protocol CMInApiHandling {
     func getNextRide(
         around coordinate: CLLocationCoordinate2D,
-        _ handler: @escaping ResultCallback<Ride?>
+        _ handler: @escaping ResultCallback<[Ride]>
     )
 }
 
@@ -25,27 +20,9 @@ struct CMInApiHandler: CMInApiHandling {
 
     func getNextRide(
         around coordinate: CLLocationCoordinate2D,
-        _ handler: @escaping ResultCallback<Ride?>
+        _ handler: @escaping ResultCallback<[Ride]>
     ) {
-        let request = NextRideRequest(coordinate: coordinate)
-        networkLayer.get(request: request) { requestResult in
-            self.sortedRidesHandler(result: requestResult, handler)
-        }
-    }
-
-    private func sortedRidesHandler(
-        result: Result<[Ride], NetworkError>,
-        _ handler: @escaping ResultCallback<Ride?>
-    ) {
-        switch result {
-        case let .success(rides):
-            let sortedRides = rides.sorted(by: \.dateTime)
-            let ride = sortedRides.first {
-                $0.dateTime > Date()
-            }
-            handler(Result.success(ride))
-        case let .failure(error):
-            handler(Result.failure(error))
-        }
+        let request = NextRidesRequest(coordinate: coordinate)
+        networkLayer.get(request: request, completion: handler)
     }
 }
