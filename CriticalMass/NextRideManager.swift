8de5342e34d62d4aa1120222cc49809d960c9e01
Malte Buenz <malte.buenz@posteo.de>
2020-01-21 11:56:22 +0100

    add nextRideManager

---
+++ CriticalMass/NextRideManager.swift
@@ -0,0 +1,61 @@
+//
+//  CriticalMaps
+
+import CoreLocation
+import Foundation
+
+struct NextRideManager {
+    private let apiHandler: CMInApiHandling
+    private let filterDistance: Double
+
+    init(apiHandler: CMInApiHandling, filterDistance: Double = 40000) {
+        self.apiHandler = apiHandler
+        self.filterDistance = filterDistance
+    }
+
+    func getNextRide(
+        around coordinate: CLLocationCoordinate2D,
+        _ handler: @escaping ResultCallback<Ride?>
+    ) {
+        apiHandler.getNextRide(around: coordinate) { requestResult in
+            self.filteredRidesHandler(result: requestResult, handler, coordinate)
+        }
+    }
+
+    private func isNextRideTooFar(_ ride: Ride, _ coordinate: CLLocationCoordinate2D) -> Bool {
+        ride.coordinate.clLocation.distance(from: coordinate.clLocation) < filterDistance
+    }
+
+    private func isNextRideInTheFuture(_ rides: [Ride]) -> Ride? {
+        rides.first { $0.dateTime > Date() }
+    }
+
+    private func filteredRidesHandler(
+        result: Result<[Ride], NetworkError>,
+        _ handler: @escaping ResultCallback<Ride?>,
+        _ coordinate: CLLocationCoordinate2D
+    ) {
+        switch result {
+        case let .success(rides):
+            guard let ride = isNextRideInTheFuture(rides) else {
+                Logger.log(.default, log: .map, "Expected ride to be in the future")
+                handler(Result.success(nil))
+                return
+            }
+            guard isNextRideTooFar(ride, coordinate) else {
+                Logger.log(.default, log: .map, "Next ride is too far away")
+                handler(Result.success(nil))
+                return
+            }
+            handler(Result.success(ride))
+        case let .failure(error):
+            handler(Result.failure(error))
+        }
+    }
+}
+
+extension CLLocationCoordinate2D {
+    var clLocation: CLLocation {
+        CLLocation(latitude: latitude, longitude: longitude)
+    }
+}
