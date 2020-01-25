f8e8a6a060b55771dd4f4b318bea0f61fc872548
Malte Buenz <malte.buenz@posteo.de>
2020-01-25 20:42:21 +0100

    remove boilerplate

--- CriticalMassTests/CoordinateObfuscatorTests.swift
+++ CriticalMassTests/CoordinateObfuscatorTests.swift
@@ -6,14 +6,6 @@ import CoreLocation
 import XCTest
 
 class CoordinateObfuscatorTests: XCTestCase {
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
     func test_ObfuscatorShouldReturnAlteredCoordinate() {
         // given
         let alexanderPlatz = CLLocationCoordinate2D.TestData.alexanderPlatz
