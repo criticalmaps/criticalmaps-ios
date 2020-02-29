c9cc329e1b6b31c95729dc0c62b12b1d2514ded6
Felizia Bernutz <felizia.bernutz@gmx.de>
2020-02-29 21:33:16 +0100

    Fix Color for Action Indicator on Dark Mode
    This was not optimal in RulesViewController and SettingsViewController.
    This fixes #175.

---
+++ CriticalMapsSnapshotTests/SettingsViewControllerSnapshotTests.swift
@@ -0,0 +1,22 @@
+//
+// Created for CriticalMaps in 2020
+
+@testable import CriticalMaps
+import XCTest
+
+class SettingsViewControllerSnapshotTests: XCTestCase {
+    private let size = CGSize(width: 320, height: 950)
+
+    func testGeneralAppearance() {
+        // Given
+        let viewController = SettingsViewController(
+            themeController: MockThemeController.shared,
+            dataStore: MockDataStore(),
+            idProvider: MockIDProvider()
+        )
+        let navigationController = UINavigationController(rootViewController: viewController)
+
+        // Then
+        assertViewSnapshot(matching: navigationController.view, with: size)
+    }
+}
