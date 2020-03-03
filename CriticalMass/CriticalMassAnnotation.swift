f69a38542096e0e14da9e232a3fb03f8a9b1a554
Malte Buenz <malte.buenz@posteo.de>
2020-03-03 17:57:53 +0100

    Add CriticalMassAnnotation

---
+++ CriticalMass/CriticalMassAnnotation.swift
@@ -0,0 +1,25 @@
+//
+//  CriticalMaps
+
+import MapKit
+
+class CriticalMassAnnotation: NSObject, MKAnnotation {
+    let ride: Ride
+
+    init(ride: Ride) {
+        self.ride = ride
+        super.init()
+    }
+
+    var title: String? {
+        ride.titleAndTime
+    }
+
+    var coordinate: CLLocationCoordinate2D {
+        ride.coordinate
+    }
+
+    var subtitle: String? {
+        ride.location
+    }
+}
