26c9f75b319023b926a752c921fcf03c2858b5bc
Malte Buenz <malte.buenz@posteo.de>
2020-03-04 20:06:52 +0100

    move extension to separate file

--- CriticalMass/Ride.swift
+++ CriticalMass/Ride.swift
@@ -28,21 +28,3 @@ extension Ride {
         return "\(titleWithoutDate)\n\(dateTime.humanReadableDate()) - \(dateTime.humanReadableTime())"
     }
 }
-
-private extension String {
-    func removedDatePattern() -> String {
-        // assuming the date is formatted like dd.MM.yyyy. Also removes the whitespace before the date
-        let pattern = " \\d{1,2}.\\d{1,2}.\\d{4}$"
-        return removedRegexMatches(pattern: pattern)
-    }
-
-    private func removedRegexMatches(pattern: String, replaceWith template: String = "") -> String {
-        do {
-            let regex = try NSRegularExpression(pattern: pattern, options: NSRegularExpression.Options.caseInsensitive)
-            let range = NSRange(location: 0, length: count)
-            return regex.stringByReplacingMatches(in: self, options: [], range: range, withTemplate: template)
-        } catch {
-            return self
-        }
-    }
-}
