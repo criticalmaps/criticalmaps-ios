26a40812d828b3d3f70362585dad9efb4ec80e07
Ilya Glushchuk <gluschuk@vl.ru>
2020-05-29 09:31:42 +1200

    Extract DataSource class for messages

---
+++ CriticalMass/MessagesTableViewDataSource.swift
@@ -0,0 +1,22 @@
+//
+// Created for CriticalMaps in 2020
+
+import UIKit
+
+final class MessagesTableViewDataSource<T: IBConstructableMessageTableViewCell>: NSObject, UITableViewDataSource {
+    var messages: [T.Model] = []
+
+    func numberOfSections(in _: UITableView) -> Int {
+        1
+    }
+
+    func tableView(_: UITableView, numberOfRowsInSection _: Int) -> Int {
+        messages.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(ofType: T.self)
+        cell.setup(for: messages[indexPath.row])
+        return cell
+    }
+}
