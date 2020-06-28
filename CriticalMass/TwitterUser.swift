01e8f50638f78f577549e3a5d2afe2fb1ffeaaa9
iglushchuk <musicguy675@gmail.com>
2020-06-28 21:45:28 +1000

    Update CriticalMass/TwitterUser.swift
    Co-authored-by: Leo Thomas <lennet@users.noreply.github.com>

--- CriticalMass/TwitterUser.swift
+++ CriticalMass/TwitterUser.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct TwitterUser: Codable, Equatable, Hashable {
+struct TwitterUser: Codable, Hashable {
     var name: String
     var screen_name: String
     var profile_image_url_https: String
