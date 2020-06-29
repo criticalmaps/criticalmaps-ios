90f8b420577bef483f65e5952ac4c40769582760
Ilya Glushchuk <gluschuk@vl.ru>
2020-06-29 23:02:27 +1000

    Add unit tests for dataSources

---
+++ CriticalMassTests/MessagesDiffableDataSourceTests.swift
@@ -0,0 +1,38 @@
+//
+// Created for CriticalMaps in 2020
+
+@testable import CriticalMaps
+import XCTest
+
+@available(iOS 13.0.0, *)
+class MessagesDiffableDataSourceTests: XCTestCase {
+    var dataSource: MessagesDiffableDataSource<ChatMessageTableViewCell>!
+    var tableView: UITableView!
+
+    override func setUpWithError() throws {
+        tableView = UITableView(frame: .zero, style: .plain)
+
+        dataSource = MessagesDiffableDataSource()
+        dataSource.configure(tableView: tableView)
+    }
+
+    override func tearDownWithError() throws {
+        dataSource = nil
+        tableView = nil
+    }
+
+    func testPopulateTableView() {
+        dataSource.messages.append(contentsOf: ChatMessage.testData)
+
+        XCTAssertTrue(tableView.numberOfSections == 1)
+        XCTAssertTrue(tableView.numberOfRows(inSection: 0) == 3)
+    }
+
+    func testClearingTableView() {
+        dataSource.messages.append(contentsOf: ChatMessage.testData)
+        dataSource.messages.removeAll()
+
+        XCTAssertTrue(tableView.numberOfSections == 1)
+        XCTAssertTrue(tableView.numberOfRows(inSection: 0) == 0)
+    }
+}
