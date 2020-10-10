e6e6670e57ff36b0744b21dea52f3fee385ce734
Felizia Bernutz <felizia.bernutz@gmx.de>
2020-10-10 09:30:59 +0200

    Fix Color Issue in Section Header

--- CriticalMass/ThemeController.swift
+++ CriticalMass/ThemeController.swift
@@ -73,8 +73,6 @@ class ThemeController {
         SettingsFooterView.appearance().versionTextColor = theme.titleTextColor
         SettingsFooterView.appearance().buildTextColor = theme.titleTextColor
         UILabel.appearance(whenContainedInInstancesOf: [SettingsInfoTableViewCell.self]).textColor = theme.titleTextColor
-        UILabel.appearance(whenContainedInInstancesOf: [SettingsTableSectionHeader.self]).backgroundColor = .clear
-        UILabel.appearance(whenContainedInInstancesOf: [UITableViewHeaderFooterView.self]).textColor = theme.titleTextColor
         SettingsSwitchTableViewCell.appearance().titleColor = theme.titleTextColor
         SettingsSwitchTableViewCell.appearance().subtitleColor = theme.thirdTitleTextColor
         SettingsCheckTableViewCell.appearance().titleColor = theme.titleTextColor
@@ -127,6 +125,7 @@ class ThemeController {
         UINavigationBar.appearance().isTranslucent = theme.navigationBarIsTranslucent
         // UIBarButtonItem
         UIBarButtonItem.appearance().tintColor = theme.titleTextColor
+        // UITableViewCell
         UITableViewCell.appearance().backgroundColor = theme.backgroundColor
         let cellSelectedBackgroundView: UIView = {
             let view = UIView()
@@ -138,12 +137,14 @@ class ThemeController {
         UIScrollView.appearance().backgroundColor = theme.backgroundColor
         // UITableViewHeaderFooterView
         UITableViewHeaderFooterView.appearance().backgroundColor = theme.backgroundColor
-        UIRefreshControl.appearance().tintColor = theme.titleTextColor
-        UITextView.appearance().textColor = theme.titleTextColor
-        // UILabel
-        UITableView.appearance().tintColor = theme.titleTextColor
-        UILabel.appearance(whenContainedInInstancesOf: [UITableViewHeaderFooterView.self]).backgroundColor = theme.backgroundColor
+        UIView.appearance(whenContainedInInstancesOf: [UITableViewHeaderFooterView.self]).backgroundColor = theme.backgroundColor
         UILabel.appearance(whenContainedInInstancesOf: [UITableViewHeaderFooterView.self]).textColor = theme.titleTextColor
+        // UIRefreshControl
+        UIRefreshControl.appearance().tintColor = theme.titleTextColor
+        // UITextView
+        UITextView.appearance().textColor = theme.titleTextColor
+        // UITableView
+        UITableView.appearance().tintColor = theme.titleTextColor
     }
 
     private func styleFriendsComponents(with theme: ThemeDefining) {
