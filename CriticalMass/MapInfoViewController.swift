ad6ad86e035cfc5326e4ce41c9ee49825462eda5
Malte Bünz <malte.buenz@deutschebahn.com>
2020-10-18 19:16:11 +0200

    set NetworkError message

--- CriticalMass/MapInfoViewController.swift
+++ CriticalMass/MapInfoViewController.swift
@@ -152,7 +152,8 @@ final class MapInfoViewController: UIViewController, IBConstructable {
         else {
             return
         }
-        errorView.setErrorLabelMessage(L10n.Map.Layer.Info.errorMessage)
+        let errorMessage = (result.isError().1 as! NetworkError).errorDescription ?? L10n.Map.Layer.Info.errorMessage
+        errorView.setErrorLabelMessage(errorMessage)
         showServerError = result.isError().0
         UIAccessibility.post(notification: .layoutChanged, argument: nil)
         showServerError ? locationUpdateErrorViewContainer.fadeIn() : locationUpdateErrorViewContainer.fadeOut()
