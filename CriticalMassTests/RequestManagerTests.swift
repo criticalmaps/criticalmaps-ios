5390882a812e12fe795ca907c13106552e59bce7
Malte Bünz <maltebuenz@posteo.de>
2021-04-20 11:32:10 +0200

    fix flaky tests

--- CriticalMassTests/RequestManagerTests.swift
+++ CriticalMassTests/RequestManagerTests.swift
@@ -23,39 +23,38 @@ class RequestManagerTests: XCTestCase {
         let setup = self.setup(interval: 0.1)
         setup.networkLayer.shouldReturnResponse = false
         XCTAssertEqual(setup.networkLayer.numberOfRequests, 0)
-        let exp = expectation(description: "Wait a second")
-        wait(interval: 1) {
-            XCTAssertEqual(setup.networkLayer.numberOfRequests, 1)
-            exp.fulfill()
-        }
-        wait(for: [exp], timeout: 4)
+
+        expectToEventually(setup.networkLayer.numberOfRequests == 1)
     }
 
     func testRespectingRequestRepeatTime() {
-        let numberOfExpectedRequests: TimeInterval = 3
+        let numberOfExpectedRequests = 3
         let interval: TimeInterval = 0.3
         let setup = self.setup(interval: interval)
         XCTAssertEqual(setup.networkLayer.numberOfRequests, 0)
-        let exp = expectation(description: "Wait a second")
-        wait(interval: interval * numberOfExpectedRequests) {
-            XCTAssertEqual(Float(setup.networkLayer.numberOfRequests), Float(numberOfExpectedRequests), accuracy: 1)
-            exp.fulfill()
-        }
-        wait(for: [exp], timeout: 4)
+
+        expectToEventually(setup.networkLayer.numberOfRequests == numberOfExpectedRequests)
     }
 
     func testStoredDataInDataStore() {
-        let expectedStorage = ApiResponse(locations: ["a": Location(longitude: 100, latitude: 100, timestamp: 100, name: "hello", color: "world")], chatMessages: ["b": ChatMessage(message: "Hello", timestamp: 1000)])
+        let expectedStorage = ApiResponse(
+            locations: [
+                "a": Location(
+                    longitude: 100,
+                    latitude: 100,
+                    timestamp: 100,
+                    name: "hello",
+                    color: "world"
+                )
+            ],
+            chatMessages: ["b": ChatMessage(message: "Hello", timestamp: 1000)]
+        )
         let setup = self.setup(interval: 0.1)
         setup.networkLayer.mockResponse = expectedStorage
         XCTAssertNil(setup.dataStore.storedData)
-        let exp = expectation(description: "Wait a second")
-        wait(interval: 1) {
-            XCTAssertNotNil(setup.dataStore.storedData)
-            XCTAssertEqual(setup.dataStore.storedData!, expectedStorage)
-            exp.fulfill()
-        }
-        wait(for: [exp], timeout: 4)
+
+        expectToEventually(setup.dataStore.storedData != nil)
+        expectToEventually(setup.dataStore.storedData! == expectedStorage)
     }
 
     func testPostLocation() {
@@ -64,27 +63,36 @@ class RequestManagerTests: XCTestCase {
         let testSetup = setup(interval: 0.1, deviceId: deviceId)
         let testLocation = Location(longitude: 100, latitude: 101, timestamp: 0, name: signature, color: nil)
         testSetup.locationProvider.mockLocation = testLocation
-        let expectedBody: [String: AnyHashable] = ["device": deviceId,
-                                                   "location": ["longitude": testLocation.longitude * 1_000_000,
-                                                                "latitude": testLocation.latitude * 1_000_000,
-                                                                "timestamp": 0,
-                                                                "name": signature as AnyHashable]]
+        let expectedBody: [String: AnyHashable] = [
+            "device": deviceId,
+            "location": [
+                "longitude": testLocation.longitude * 1_000_000,
+                "latitude": testLocation.latitude * 1_000_000,
+                "timestamp": 0,
+                "name": signature as AnyHashable
+            ]
+        ]
         XCTAssertNil(testSetup.dataStore.storedData)
-        let exp = expectation(description: "Wait a second")
-        wait(interval: 1) {
-            XCTAssertEqual(testSetup.networkLayer.numberOfGetCalled, 0)
-            XCTAssertGreaterThanOrEqual(testSetup.networkLayer.numberOfPostCalled, 1)
-            XCTAssertEqual(testSetup.networkLayer.lastUsedPostBody as! [String: AnyHashable], expectedBody)
-            exp.fulfill()
-        }
-        wait(for: [exp], timeout: 4)
+
+        expectToEventually(testSetup.networkLayer.numberOfGetCalled == 0)
+        expectToEventually(testSetup.networkLayer.numberOfPostCalled >= 1)
+        expectToEventually(testSetup.networkLayer.lastUsedPostBody as? [String: AnyHashable] == expectedBody)
     }
 
     func testSendMessage() {
         let deviceId = "123456789"
         let testSetup = setup(interval: 0.1, deviceId: deviceId)
         let testMessage = SendChatMessage(text: "Hello", timestamp: 100, identifier: UUID().uuidString)
-        let expectedBody: [String: AnyHashable] = ["device": deviceId, "messages": [["text": testMessage.text, "timestamp": testMessage.timestamp, "identifier": testMessage.identifier]] as! [[String: AnyHashable]]]
+        let expectedBody: [String: AnyHashable] = [
+            "device": deviceId,
+            "messages": [
+                [
+                    "text": testMessage.text,
+                    "timestamp": testMessage.timestamp,
+                    "identifier": testMessage.identifier
+                ]
+            ] as! [[String: AnyHashable]]
+        ]
         XCTAssertNil(testSetup.dataStore.storedData)
         testSetup.requestManager.send(messages: [testMessage]) { _ in }
 
@@ -95,51 +103,32 @@ class RequestManagerTests: XCTestCase {
 
     // this test is flaky and should be made more tolerant
     // issue: https://github.com/criticalmaps/criticalmaps-ios/issues/266
-    func disabledtestNetworkSatisfiedStatusUpdate() {
+    func test_NetworkSatisfiedStatusUpdate() {
         let setup = self.setup(interval: 4)
         setup.networkObserver.update(with: .satisfied)
 
-        let exp = expectation(description: "Wait a second")
-        wait(interval: 1) {
-            XCTAssertEqual(setup.networkLayer.numberOfGetCalled, 0)
-            XCTAssertEqual(setup.networkLayer.numberOfPostCalled, 1)
-
-            exp.fulfill()
-        }
-        wait(for: [exp], timeout: 4)
+        expectToEventually(setup.networkLayer.numberOfGetCalled == 0)
+        expectToEventually(setup.networkLayer.numberOfPostCalled == 1)
     }
 
     func testNetworkNoneStatusUpdate() {
         let setup = self.setup(interval: 4)
         setup.networkObserver.update(with: .none)
 
-        let exp = expectation(description: "Wait a second")
-        wait(interval: 1) {
-            XCTAssertEqual(setup.networkLayer.numberOfGetCalled, 0)
-            XCTAssertEqual(setup.networkLayer.numberOfPostCalled, 0)
-
-            exp.fulfill()
-        }
-        wait(for: [exp], timeout: 4)
+        expectToEventually(setup.networkLayer.numberOfGetCalled == 0)
+        expectToEventually(setup.networkLayer.numberOfPostCalled == 0)
     }
 
     func testRegainingNetworkAcccess() {
         let setup = self.setup(interval: 4)
         setup.networkObserver.update(with: .none)
 
-        let exp = expectation(description: "Wait a second")
-        wait(interval: 1) {
-            XCTAssertEqual(setup.networkLayer.numberOfGetCalled, 0)
-            XCTAssertEqual(setup.networkLayer.numberOfPostCalled, 0)
+        expectToEventually(setup.networkLayer.numberOfGetCalled == 0)
+        expectToEventually(setup.networkLayer.numberOfPostCalled == 0)
 
-            setup.networkObserver.update(with: .satisfied)
-            self.wait(interval: 1) {
-                XCTAssertEqual(setup.networkLayer.numberOfGetCalled, 0)
-                XCTAssertEqual(setup.networkLayer.numberOfPostCalled, 1)
+        setup.networkObserver.update(with: .satisfied)
 
-                exp.fulfill()
-            }
-        }
-        wait(for: [exp], timeout: 4)
+        expectToEventually(setup.networkLayer.numberOfGetCalled == 0)
+        expectToEventually(setup.networkLayer.numberOfPostCalled == 1)
     }
 }
--- CriticalMassTests/XCTestCase+Helper.swift
+++ CriticalMassTests/XCTestCase+Helper.swift
@@ -21,4 +21,21 @@ extension XCTestCase {
             function()
         }
     }
+
+    /// https://www.vadimbulavin.com/swift-asynchronous-unit-testing-with-busy-assertion-pattern/
+    func expectToEventually(
+        _ test: @autoclosure () -> Bool,
+        timeout: TimeInterval = 1.0,
+        message: String = ""
+    ) {
+        let runLoop = RunLoop.current
+        let timeoutDate = Date(timeIntervalSinceNow: timeout)
+        repeat {
+            if test() {
+                return
+            }
+            runLoop.run(until: Date(timeIntervalSinceNow: 0.01))
+        } while Date().compare(timeoutDate) == .orderedAscending
+        XCTFail(message)
+    }
 }
