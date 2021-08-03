ef00e4d199ee4837ba53ab341071c892370e546b
Malte Bünz <maltebuenz@posteo.de>
2021-08-03 18:31:29 +0200

    fix container viewController

--- CriticalMapsKit/Sources/InfoBar/Content/InfobarContainerViewController.swift
+++ CriticalMapsKit/Sources/InfoBar/Content/InfobarContainerViewController.swift
@@ -14,7 +14,7 @@ public final class InfobarContainerViewController: UIViewController, StatusbarSt
     
     addChild(rootViewController)
     view.addSubview(rootViewController.view)
-    rootViewController.additionalSafeAreaInsets = .init(top: 0, left: 0, bottom: 200, right: 0)
+    rootViewController.additionalSafeAreaInsets = .init(top: 0, left: 0, bottom: 50, right: 0)
     rootViewController.view.translatesAutoresizingMaskIntoConstraints = false
     rootViewController.view.backgroundColor = .clear
     NSLayoutConstraint.activate(
@@ -29,7 +29,7 @@ public final class InfobarContainerViewController: UIViewController, StatusbarSt
           equalTo: view.safeAreaLayoutGuide.trailingAnchor
         ),
         rootViewController.view.bottomAnchor.constraint(
-          equalTo: view.safeAreaLayoutGuide.bottomAnchor
+          equalTo: view.bottomAnchor
         )
       ]
     )
