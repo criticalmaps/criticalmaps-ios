c95464627c80808af7424780859a9da89bfd2835
Malte Bünz <maltebuenz@posteo.de>
2021-11-24 19:38:14 +0100

    add Blur View

---
+++ CriticalMapsKit/Sources/Styleguide/Blur.swift
@@ -0,0 +1,17 @@
+import SwiftUI
+
+public struct Blur: UIViewRepresentable {
+  var style: UIBlurEffect.Style = .systemMaterial
+
+  public init(style: UIBlurEffect.Style = .systemMaterial) {
+    self.style = style
+  }
+    
+  public func makeUIView(context: Context) -> UIVisualEffectView {
+    return UIVisualEffectView(effect: UIBlurEffect(style: style))
+  }
+  
+  public func updateUIView(_ uiView: UIVisualEffectView, context: Context) {
+    uiView.effect = UIBlurEffect(style: style)
+  }
+}
