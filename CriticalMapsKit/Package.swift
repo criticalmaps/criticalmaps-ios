4816f04816d1b28e97b179ec920a25b1ee9245e8
Malte Bünz <maltebuenz@posteo.de>
2021-12-04 20:48:25 +0100

    bump to ios 15

--- CriticalMapsKit/Package.swift
+++ CriticalMapsKit/Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.5
 
 import PackageDescription
 
@@ -6,7 +6,7 @@ let package = Package(
   name: "CriticalMapsKit",
   defaultLocalization: "en",
   platforms: [
-    .iOS(.v14)
+    .iOS(.v15)
   ],
   products: [
     .library(name: "ApiClient", targets: ["ApiClient"]),
