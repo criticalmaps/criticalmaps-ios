ea53777415705105c43fff66f99101d5034f8077
Malte Bünz <maltebuenz@posteo.de>
2021-12-07 13:10:26 +0100

    add chatViewState tests

---
+++ CriticalMapsKit/Tests/ChatFeatureTests/ChatViewStateTests.swift
@@ -0,0 +1,31 @@
+import ChatFeature
+import Foundation
+import SharedModels
+import XCTest
+
+class ChatViewStateTests: XCTestCase {
+  func test_createChatViewState_shouldSortMessagesByDate() {
+    // arrange
+    let chatFeatureState = ChatFeatureState(
+      chatMessages: [
+        "1": .init(message: "Hello", timestamp: 1),
+        "2": .init(message: "Hello", timestamp: 2),
+        "3": .init(message: "Hello", timestamp: 3),
+      ],
+      chatInputState: .init(),
+      hasConnectivity: true
+    )
+    
+    // act
+    let chatViewState = ChatView.ChatViewState(chatFeatureState)
+    let messages = chatViewState.identifiedChatMessages
+    
+    // assert
+    let expectedMessages: [IdentifiedChatMessage] = [
+      .init(id: "3", message: "Hello", timestamp: 3),
+      .init(id: "2", message: "Hello", timestamp: 2),
+      .init(id: "1", message: "Hello", timestamp: 1)
+    ]
+    XCTAssertEqual(messages, expectedMessages)
+  }
+}
