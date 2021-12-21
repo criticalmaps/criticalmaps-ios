5e1a1c3d733ffd237a76eca556122cacf584e22c
Malte Bünz <maltebuenz@posteo.de>
2021-12-21 22:50:57 +0100

    add chatfeature preview app

---
+++ ChatFeaturePreviewApp/ChatFeaturePreviewApp.swift
@@ -0,0 +1,26 @@
+import ChatFeature
+import SwiftUI
+
+@main
+struct ChatFeaturePreviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+          NavigationView {
+            ChatView(
+              store: .init(
+                initialState: .init(),
+                reducer: chatReducer,
+                environment: .init(
+                  locationsAndChatDataService: .live(),
+                  mainQueue: .main,
+                  idProvider: .live(),
+                  uuid: UUID.init,
+                  date: Date.init,
+                  userDefaultsClient: .live()
+                )
+              )
+            )
+          }
+        }
+    }
+}
