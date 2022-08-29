67c6479cd07daa27aec2142d677afb2f1d8d67f8
Malte Bünz <maltebuenz@posteo.de>
2022-08-29 13:45:57 +0200

    update timer core

--- CriticalMapsKit/Sources/AppFeature/RequestTimerCore.swift
+++ CriticalMapsKit/Sources/AppFeature/RequestTimerCore.swift
@@ -1,19 +1,26 @@
 import ComposableArchitecture
 
 public struct RequestTimerState: Equatable {
-  public init() {}
+  public init(isTimerActive: Bool = false) {
+    self.isTimerActive = isTimerActive
+  }
+  
+  var isTimerActive = false
 }
 
 public enum RequestTimerAction: Equatable {
   case timerTicked
   case startTimer
-  case stopTimer
 }
 
 public struct RequestTimerEnvironment {
   let timerInterval: Double
   let mainQueue: AnySchedulerOf<DispatchQueue>
 
+  var interval: DispatchQueue.SchedulerTimeType.Stride {
+    DispatchQueue.SchedulerTimeType.Stride(floatLiteral: timerInterval)
+  }
+  
   public init(timerInterval: Double = 12.0, mainQueue: AnySchedulerOf<DispatchQueue>) {
     self.timerInterval = timerInterval
     self.mainQueue = mainQueue
@@ -21,7 +28,7 @@ public struct RequestTimerEnvironment {
 }
 
 /// Reducer responsible for the poll timer handling.
-public let requestTimerReducer = Reducer<RequestTimerState, RequestTimerAction, RequestTimerEnvironment> { _, action, environment in
+public let requestTimerReducer = Reducer<RequestTimerState, RequestTimerAction, RequestTimerEnvironment> { state, action, environment in
   struct TimerId: Hashable {}
 
   switch action {
@@ -29,15 +36,13 @@ public let requestTimerReducer = Reducer<RequestTimerState, RequestTimerAction, 
     return .none
 
   case .startTimer:
-    return Effect.timer(
-      id: TimerId(),
-      every: .seconds(environment.timerInterval),
-      on: environment.mainQueue
-    )
-    .map { _ in .timerTicked }
-
-  // needed for tests mainly
-  case .stopTimer:
-    return .cancel(id: TimerId())
+    state.isTimerActive = true
+    return .run { [isTimerActive = state.isTimerActive] send in
+      guard isTimerActive else { return }
+      for await _ in environment.mainQueue.timer(interval: environment.interval) {
+        await send(.timerTicked)
+      }
+    }
+    .cancellable(id: TimerId.self, cancelInFlight: true)
   }
 }
