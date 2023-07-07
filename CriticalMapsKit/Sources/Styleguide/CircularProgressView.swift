dbd8d90ba1d99513e37c08d6056442a2dfad688e
Malte Bünz <maltebuenz@posteo.de>
2023-07-07 23:21:14 +0200

    add circularProgressView

---
+++ CriticalMapsKit/Sources/Styleguide/CircularProgressView.swift
@@ -0,0 +1,36 @@
+import SwiftUI
+
+public struct CircularProgressView: View {
+  let progress: Double
+  
+  public init(progress: Double) {
+    self.progress = progress
+  }
+  
+  public var body: some View {
+    ZStack {
+      Circle()
+        .stroke(
+          Color(.brand500).opacity(0.4),
+          lineWidth: 8
+        )
+      
+      Circle()
+        .trim(from: 0, to: progress)
+        .stroke(
+          Color(.brand500),
+          style: StrokeStyle(
+            lineWidth: 8,
+            lineCap: .round
+          )
+        )
+        .rotationEffect(.degrees(-90))
+    }
+  }
+}
+
+struct CircularProgressView_Previews: PreviewProvider {
+  static var previews: some View {
+    CircularProgressView(progress: 0.4)
+  }
+}
