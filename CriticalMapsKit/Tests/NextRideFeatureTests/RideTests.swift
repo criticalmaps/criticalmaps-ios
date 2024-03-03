1fdc3c60f05f6ccad51551425ec5bc39a498f12d
Malte Bünz <maltebuenz@posteo.de>
2024-03-03 20:21:33 +0100

    update tests

--- CriticalMapsKit/Tests/NextRideFeatureTests/RideTests.swift
+++ CriticalMapsKit/Tests/NextRideFeatureTests/RideTests.swift
@@ -1,4 +1,5 @@
 import ComposableArchitecture
+import Helpers
 import SharedModels
 import XCTest
 
@@ -12,7 +13,7 @@ final class RideTests: XCTestCase {
       $0.timeZone = .gmt
     } operation: {
       XCTAssertEqual(
-        "19:00",
+        "20:00",
         ride.dateTime.humanReadableTime
       )
     }
@@ -43,7 +44,7 @@ final class RideTests: XCTestCase {
       $0.calendar = .init(identifier: .gregorian)
     } operation: {
       XCTAssertEqual(
-        "21:00",
+        "20:00",
         ride.dateTime.humanReadableTime
       )
     }
@@ -59,7 +60,7 @@ final class RideTests: XCTestCase {
       $0.calendar = .init(identifier: .gregorian)
     } operation: {
       XCTAssertEqual(
-        "14:00",
+        "20:00",
         ride.dateTime.humanReadableTime
       )
     }
@@ -86,29 +87,4 @@ extension Ride {
       rideType: .criticalMass
     )
   }
-}
-
-extension TimeZone {
-  // Europe
-  static let germany = TimeZone(identifier: "Europe/Berlin")!
-  static let gmt = TimeZone(identifier: "GMT")!
-  static let spain = TimeZone(identifier: "Europe/Madrid")!
-  static let france = TimeZone(identifier: "Europe/Paris")!
-  static let greece = TimeZone(identifier: "Europe/Athens")!
-  
-  // America
-  static let ecuador = TimeZone(identifier: "America/Guayaquil")!
-  static let cuba = TimeZone(identifier: "America/Havana")!
-  
-  // Africa
-  static let egypt = TimeZone(identifier: "Africa/Cairo")!
-  static let southAfrica = TimeZone(identifier: "Africa/Johannesburg")!
-  
-  // Asia
-  static let india = TimeZone(identifier: "Asia/Kolkata")!
-  static let japan = TimeZone(identifier: "Asia/Tokyo")!
-  
-  // USA
-  static let newYork = TimeZone(identifier: "America/New_York")!
-  static let losAngeles = TimeZone(identifier: "America/Los_Angeles")!
 }
