7a5d40633aafa98ee82b89b448239c52053458cf
Malte Bünz <maltebuenz@posteo.de>
2024-10-03 00:13:27 +0200

    extract view

---
+++ CriticalMapsKit/Sources/AppFeature/RideEventView.swift
@@ -0,0 +1,73 @@
+import Foundation
+import SharedModels
+import Styleguide
+import SwiftUI
+
+struct RideEventView: View {
+  let ride: Ride
+  
+  var body: some View {
+    HStack(alignment: .center, spacing: .grid(2)) {
+      Image(uiImage: Asset.cm.image)
+        .scaleEffect(0.8)
+        .aspectRatio(contentMode: .fit)
+        .shadow(color: .black.opacity(0.3), radius: 3)
+        .clipped()
+        .accessibilityHidden(true)
+      VStack(alignment: .leading, spacing: .grid(1)) {
+        Text(ride.title)
+          .multilineTextAlignment(.leading)
+          .font(Font.body.weight(.semibold))
+          .foregroundColor(Color(.textPrimary))
+          .padding(.bottom, .grid(2))
+        
+        VStack(alignment: .leading, spacing: 2) {
+          Label(ride.dateTime.humanReadableDate, systemImage: "calendar")
+            .multilineTextAlignment(.leading)
+            .font(.bodyTwo)
+            .foregroundColor(Color(.textSecondary))
+          
+          Label(ride.rideTime, systemImage: "clock")
+            .multilineTextAlignment(.leading)
+            .font(.bodyTwo)
+            .foregroundColor(Color(.textSecondary))
+          
+          if let location = ride.location {
+            Label(location, systemImage: "location.fill")
+              .multilineTextAlignment(.leading)
+              .font(.bodyTwo)
+              .foregroundColor(Color(.textSecondary))
+          }
+        }
+      }
+      Spacer()
+    }
+  }
+}
+
+#Preview {
+  RideEventView(
+    ride: Ride(
+      id: 0,
+      city: Ride.City(
+        id: 1,
+        name: "Berlin",
+        timezone: TimeZone(identifier: "Europe/Berlin")!.identifier
+      ),
+      slug: nil,
+      title: "CriticalMaps Berlin",
+      description: nil,
+      dateTime: Date(timeIntervalSince1970: 1727905659),
+      location: "Berlin",
+      latitude: 53.1235,
+      longitude: 13.4234,
+      estimatedParticipants: nil,
+      estimatedDistance: nil,
+      estimatedDuration: nil,
+      enabled: true,
+      disabledReason: nil,
+      disabledReasonMessage: nil,
+      rideType: .criticalMass
+    )
+  )
+}
