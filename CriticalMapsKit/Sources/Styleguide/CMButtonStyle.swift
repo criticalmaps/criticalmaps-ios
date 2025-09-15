0d628b8c537ec99f671f6fded8ad3660f517187e
Malte Bünz <maltebuenz@posteo.de>
2025-09-15 16:24:05 +0200

    update viewcomponents

--- CriticalMapsKit/Sources/Styleguide/CMButtonStyle.swift
+++ CriticalMapsKit/Sources/Styleguide/CMButtonStyle.swift
@@ -2,6 +2,8 @@ import SwiftUI
 
 /// CM default ButtonStyle
 public struct CMButtonStyle: ButtonStyle {
+  @Environment(\.isEnabled) var isEnabled
+  
   public init() {}
 
   public func makeBody(configuration: Configuration) -> some View {
@@ -12,14 +14,36 @@ public struct CMButtonStyle: ButtonStyle {
           ? Color(.textPrimaryLight).opacity(0.6)
           : Color(.textPrimaryLight)
       )
-      .scaleEffect(configuration.isPressed ? 0.9 : 1)
+      .font(.body)
       .padding(.horizontal, .grid(4))
-      .padding(.vertical, .grid(2))
-      .background(Color(.brand500))
-      .cornerRadius(8)
+      .padding(.vertical, .grid(4))
+      .background(
+        Color(.brand500)
+          .opacity(isEnabled ? 1.0 : 0.4)
+      )
+      .if(!.iOS26) { view in
+        view.clipShape(.rect(cornerRadius: .grid(2)))
+      }
+      .if(.iOS26) { view in
+        view.clipShape(.capsule)
+      }
+      .scaleEffect(configuration.isPressed ? 0.96 : 1)
       .accessibleAnimation(
-        .snappy,
+        .snappy(duration: 0.24),
         value: configuration.isPressed
       )
+  }
+}
+
+public struct GlassButtonModifier: ViewModifier {
+  public init() {}
+  
+  public func body(content: Content) -> some View {
+    if #available(iOS 26, *) {
+      content
+        .buttonStyle(.glass)
+    } else {
+      content
+    }
   }
 }
