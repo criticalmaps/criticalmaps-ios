686371bbc888f8e36a26fd3d8cc56c66c3b1e20d
Malte Bünz <maltebuenz@posteo.de>
2026-01-03 21:55:04 +0100

    add extensions

--- CriticalMapsKit/Sources/TestHelper/SnapshotHelper.swift
+++ CriticalMapsKit/Sources/TestHelper/SnapshotHelper.swift
@@ -16,7 +16,7 @@ public enum SnapshotHelper {
   @MainActor
   public static func assertScreenSnapshot(
     _ view: some View,
-    sloppy: Bool = false,
+    sloppy: Bool = true,
     file: StaticString = #filePath,
     testName: String = #function,
     line: UInt = #line
@@ -32,8 +32,8 @@ public enum SnapshotHelper {
           .image(
             precision: precision,
             perceptualPrecision: precision,
-            layout: .device(config: .iPhone13),
-            traits: .iPhone13(.portrait)
+            layout: .device(config: .iPhone16),
+            traits: .iPhone16(.portrait)
           )
         ],
         file: file,
@@ -55,7 +55,7 @@ public enum SnapshotHelper {
   ) throws {
     try enforceSnapshotDevice()
     
-    var layout = SwiftUISnapshotLayout.device(config: .iPhone8)
+    var layout = SwiftUISnapshotLayout.device(config: .iPhone16)
     if let height {
       layout = .fixed(width: width, height: height)
     }
@@ -83,4 +83,66 @@ private let operatingSystemVersion = ProcessInfo().operatingSystemVersion
 
 public enum SnapshotError: Error {
   case deviceNot2XOrNotIOS16
+}
+
+public extension ViewImageConfig {
+  static let iPhone16 = ViewImageConfig.iPhone16(.portrait)
+	
+  static func iPhone16(_ orientation: Orientation) -> ViewImageConfig {
+    let safeArea: UIEdgeInsets
+    let size: CGSize
+    switch orientation {
+    case .landscape:
+      safeArea = .init(top: 0, left: 59, bottom: 21, right: 59)
+      size = .init(width: 852, height: 393)
+    case .portrait:
+      safeArea = .init(top: 59, left: 0, bottom: 34, right: 0)
+      size = .init(width: 393, height: 852)
+    }
+    return .init(safeArea: safeArea, size: size, traits: .iPhone16(orientation))
+  }
+}
+
+public extension UITraitCollection {
+  static func iPhone16(_ orientation: ViewImageConfig.Orientation) -> UITraitCollection {
+    if #available(iOS 17.0, *) {
+      return UITraitCollection(mutations: { mutableTraits in
+        mutableTraits.forceTouchCapability = .unavailable
+        mutableTraits.layoutDirection = .leftToRight
+        mutableTraits.preferredContentSizeCategory = .medium
+        mutableTraits.userInterfaceIdiom = .phone
+        switch orientation {
+        case .landscape:
+          mutableTraits.horizontalSizeClass = .compact
+          mutableTraits.verticalSizeClass = .compact
+        case .portrait:
+          mutableTraits.horizontalSizeClass = .compact
+          mutableTraits.verticalSizeClass = .regular
+        }
+      })
+    } else {
+      let base: [UITraitCollection] = [
+        .init(forceTouchCapability: .unavailable),
+        .init(layoutDirection: .leftToRight),
+        .init(preferredContentSizeCategory: .medium),
+        .init(userInterfaceIdiom: .phone)
+      ]
+      switch orientation {
+      case .landscape:
+        return .init(
+          traitsFrom: base + [
+            .init(horizontalSizeClass: .compact),
+            .init(verticalSizeClass: .compact)
+          ]
+        )
+      case .portrait:
+        return .init(
+          traitsFrom: base + [
+            .init(horizontalSizeClass: .compact),
+            .init(verticalSizeClass: .regular)
+          ]
+        )
+      }
+    }
+  }
 }
